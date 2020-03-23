dda59e475220ed3d91acb4739c11147e8d8ac48a
Aditya Chaturvedi <aditya.chaturvedi@zensar.com>
2020-03-24 02:01:44 +0530

    WIP, render playlist

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
     Button,
     Modal,
@@ -15,26 +15,45 @@ import axios from "axios";
 
 export default class PlayListView extends Component {
     constructor(props) {
-        super(props);  
-    }
-    state = {
-        all_files: []
+        super(props);
+        this.state = {
+            allFiles: [],
+        };
     }
 
-    requestForAllFiles(event) {
-        event.preventDefault();
+    requestForAllFiles = () => {
         let custom_url = "http://localhost:8000/api/all_files/"
         axios
-        .get(custom_url)
-        .then(response =>  this.setState({all_files: response.data}))
-        .catch(error => console.log(error))
+            .get(custom_url)
+            .then(response => this.setState({ allFiles: response.data }))
+            .catch(error => console.log(error))
+    }
+
+    renderPlaylist = () => {
+        //WIP
+        const allItems = this.state.allFiles
+        console.log(allItems, "11111111")
+        return allItems.map(item => (
+            <li 
+                className="list-group-item d-flex justify-content-between align-items-center">
+                    <span className="list-group-item" title={item}>
+                        {item}
+                        </span> 
+                </li>
+
+        ))
     }
 
     render() {
-        return(
-        <form onSubmit={this.requestForAllFiles}>
-            <Button variant="outline-dark" type="submit">All Files</Button>
-        </form>    
+        return (
+            <div>
+                <form onSubmit={this.requestForAllFiles}>
+                    <Button variant="outline-dark" type="submit">All Files</Button>
+                </form>
+                <ul className="list-group list-group-flush">
+                    {this.renderPlaylist()}
+                </ul>
+            </div>
         )
     }
 }
